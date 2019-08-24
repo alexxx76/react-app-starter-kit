14216b3a50120f07fa6dfabdc158df1169f54ac0
Alexander Karpenko <alexxx76gamer@gmail.com>
2019-08-24 13:44:33 +0500

    Loading fonts

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import './style.css';
 import FlowersImage from './flowers.png';
 import CatsImage from './cats.jpg';
 
-const el = document.querySelector('#root');
-el.classList.add('hello');
+const root = document.querySelector('#root');
+root.classList.add('hello');
 
 const Flowers = new Image();
 Flowers.src = FlowersImage;
-el.append(Flowers);
+root.append(Flowers);
 Flowers.width = 50;
 
 const Cats = new Image();
 Cats.src = CatsImage;
-el.append(Cats);
+root.append(Cats);
 Cats.width = 100;
+
+const p = document.createElement('div');
+root.append(p);
+p.innerHTML = 'Yellow';
 
 alert('Hello world!');
