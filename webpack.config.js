63a701a8bc146d8af91f549d60c8021bfaa27d50
Alexander Karpenko <alexxx76gamer@gmail.com>
2019-08-24 17:16:12 +0500

    Add webpack dev server - npm start

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,9 @@ module.exports = {
     print: './src/print.js'
   },
   devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './public'
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
