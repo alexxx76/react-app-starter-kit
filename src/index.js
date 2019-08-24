1fc8dbcfbb56425da6c167d55413ff08055d741a
Alexander Karpenko <alexxx76gamer@gmail.com>
2019-08-24 12:29:45 +0500

    Loading images

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import './style.css';
+import FlowersImage from './flowers.png';
+import CatsImage from './cats.jpg';
 
 const el = document.querySelector('#root');
 el.classList.add('hello');
+
+const Flowers = new Image();
+Flowers.src = FlowersImage;
+el.append(Flowers);
+Flowers.width = 50;
+
+const Cats = new Image();
+Cats.src = CatsImage;
+el.append(Cats);
+Cats.width = 100;
 
 alert('Hello world!');
