c942a685e9aa19f68a0f0e5d49d50ae8b3394835
Alexander Karpenko <alexxx76gamer@gmail.com>
2019-08-24 14:43:02 +0500

    Output preparation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
+import printMe from './print.js';
+
 function component() {
   console.log('component');
   const element = document.createElement('div');
+  const btn = document.createElement('button');
+
   element.innerHTML = 'Hello webpack';
+  btn.innerHTML = 'Click me and check the console!';
+
+  btn.onclick = printMe;
+
+  element.append(btn);
+
   return element;
 }
 
