e76dafad5c71d516754aadf70c91fd52ae816e8c
Alexander Karpenko <alexxx76gamer@gmail.com>
2019-08-24 14:26:53 +0500

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
-import './style.css';
-import FlowersImage from './flowers.png';
-import CatsImage from './cats.jpg';
-import Data from './data.xml';
+function component() {
+  console.log('component');
+  const element = document.createElement('div');
+  element.innerHTML = 'Hello webpack';
+  return element;
+}
 
 const root = document.querySelector('#root');
-root.classList.add('hello');
-
-const Flowers = new Image();
-Flowers.src = FlowersImage;
-root.append(Flowers);
-Flowers.width = 50;
-
-const Cats = new Image();
-Cats.src = CatsImage;
-root.append(Cats);
-Cats.width = 100;
-
-const p = document.createElement('div');
-root.append(p);
-p.innerHTML = 'Yellow';
-
-console.log(Data);
-
-alert('Hello world!');
+root.append(component());
